ba8ffd57bc24652fd2a13ceec0d8f53759a3707b
Aliii666 <aliyhamy04@gmail.com>
2026-07-10 08:10:03 -0500

    SHOPAPP CHECKPOINT  - CONFIG - 10-07-2026

--- modulo4/reactjs/01_components/src/main.tsx
+++ modulo4/reactjs/01_components/src/main.tsx
@@ -1,25 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import { ThemeProvider } from './contexts/ThemeContext.tsx'
-import { AuthProvider }  from './contexts/AuthContext.tsx'
-import { LanguageProvider } from './contexts/LanguageContext.tsx'
-import { NotificationProvider } from './contexts/NotificationContext.tsx'
-import { CartProvider } from './contexts/CartContext.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <LanguageProvider>
-          <NotificationProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </NotificationProvider>
-        </LanguageProvider>
-      </AuthProvider>
-    </ThemeProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
